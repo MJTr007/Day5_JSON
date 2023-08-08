81241c041ceec9bec35edfdf9d63b715f7c3b8dd
MJTr007 <manojram19944@gmail.com>
2023-08-08 10:17:47 +0530

    Day5_JSON

---
+++ script.js
@@ -0,0 +1,47 @@
+var resumeData = {
+    "name": "Manoj",
+    "education": "BE.Mechanical engineering",
+    "gender" : "Male",
+    "languages known" : [
+        "Tamil",
+        "English",
+    ],
+    "city": "Coimbatore",
+    "contact": "*******888",
+    "email": "*****@gmail.com",
+    "skills": [
+      "Full stack development",
+      "AWS",
+      "React js",
+      "Node js",
+      "Mongo DB",
+      "Data Structure Algorithm"
+    ]
+  };
+
+  console.log(resumeData);
+  
+//   // Using for loop
+//   console.log("Using for loop:");
+//   
+  
+//   // Using for...in loop
+//   console.log("Using for...in loop:");
+//   for (var key in resumeData) {
+//     console.log(key + ": " + resumeData[key]);
+//   }
+//   console.log("");
+  
+//   // Using for...of loop (only for skills array)
+//   console.log("Using for...of loop:");
+//   for (var skill of resumeData.skills) {
+//     console.log(skill);
+//   }
+//   console.log("");
+  
+//   // Using forEach loop (only for skills array)
+//   console.log("Using forEach loop:");
+//   resumeData.skills.forEach(function(skill) {
+//     console.log(skill);
+//   });
+  
