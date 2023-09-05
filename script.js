2dcb511e01358fe8ca49fd9a0f644f4a3ba9a397
MJTr007 <manojram19944@gmail.com>
2023-09-05 11:08:58 +0530

    update

--- script.js
+++ script.js
@@ -19,27 +19,33 @@ var resumeData = {
     ]
   };
 
-  console.log(resumeData);
+  //console.log(resumeData);
   
-//   // Using for loop
+//   // Using "for loop"
 //   console.log("Using for loop:");
+     for(var i = 0; i< resumeData.length; i++){
+      var object = json[i];
+
+      console.log(object.name);
+     }
+
 //   
   
-//   // Using for...in loop
+//   // Using "for in" loop
 //   console.log("Using for...in loop:");
 //   for (var key in resumeData) {
 //     console.log(key + ": " + resumeData[key]);
 //   }
 //   console.log("");
   
-//   // Using for...of loop (only for skills array)
+//   // Using "for of" loop (only for skills array)
 //   console.log("Using for...of loop:");
 //   for (var skill of resumeData.skills) {
 //     console.log(skill);
 //   }
 //   console.log("");
   
-//   // Using forEach loop (only for skills array)
+//   // Using "for Each loop" (only for skills array)
 //   console.log("Using forEach loop:");
 //   resumeData.skills.forEach(function(skill) {
 //     console.log(skill);
